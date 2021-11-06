9621176356ffd83d63a5b8f82dae6596a501fb1b
James Rowe <james.r@weareframework.co.uk>
2021-11-06 15:29:48 +0000

    fix for prod

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,15 +1,7 @@
 const mix = require('laravel-mix')
 
 mix
-  .js('resources/js/infinite-marquee.js', 'assets/js')
   .js('resources/js/demo.js', 'assets/js')
-  .webpackConfig({
-    output: {
-      library: 'InfiniteMarquee',
-      libraryTarget: 'umd',
-      umdNamedDefine: true
-    }
-  })
   .postCss('resources/css/infinite-marquee.css', 'assets/css')
   .options({
     processCssUrls: false
